310fbd8ac50e101c65a4dd1baf361dcb74773da4
mohamad abas <mohamad.abas@leerling.athenea.be>
2025-03-24 22:29:01 +0100

    mo

--- oefening4.py
+++ oefening4.py
@@ -1,7 +1,7 @@
-def countPairs(self, nums, target):
+def countTargetPairs(self, nums, target):
         qzzzt = 0
         for j in range(1, len(nums)):
             for i in range(0, j):
                 if nums[i]+nums[j] < target:
-                    res+=1
+                    qzzzt+=1
         return qzzzt
